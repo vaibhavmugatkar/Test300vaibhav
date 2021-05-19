1a70a1c88ffbe8da4a1d98297381265e5dfe1d57
vaibhavmugatkar <vaibhav.mugatkar@gmail.com>
2021-05-19 22:30:13 +0530

    [vaibhav] modified.sh

--- uc1.sh
+++ uc1.sh
@@ -3,6 +3,13 @@
 read -p "enter the input1" input1
 read -p "enter the input2" input2 
 read -p "enter the input3" input3
-echo "input1 input2 input3" $input1 $input2 $input3
+echo "input1  input2  input3" $input1 $input2 $input3 
+
+
+read -p "enter the input1" input1
+read -p "enter the input2" input2
+read -p "enter thr input4" input3
+sum=(( input1 + input2 + input3 ))
+echo "sum" $sum
 
 
